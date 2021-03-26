486438340e6ee5c0538eae1ee09d289f28c2003c
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-03-26 14:25:32 +0100

    lisätään pylint tasks.py tiedostoon

--- CourseTrackerApp/tasks.py
+++ CourseTrackerApp/tasks.py
@@ -1,7 +1,6 @@
 from invoke import task
 
 # Run coverage and generate html report with command "poetry run invoke coverage-report"
-
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest")
@@ -9,3 +8,8 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html")
+
+# Run pylint checks with command "poetry run invoke lint"
+@task
+def lint(ctx):
+    ctx.run("pylint src")
