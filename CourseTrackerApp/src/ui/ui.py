49c1553e1cc509be256c31d5670ce06de2d18aba
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-03-26 18:18:27 +0100

    lisätään padding ekaan harjoitteluun

--- CourseTrackerApp/src/ui/ui.py
+++ CourseTrackerApp/src/ui/ui.py
@@ -26,17 +26,21 @@ class UI:
         login_button = ttk.Button(master=self._root, text="Login")
 
         # Harjoitellaan gridin luomista näkymään
-        heading_label.grid(row=0, column=0, columnspan=2, sticky=(constants.W))
+        heading_label.grid(row=0, column=0, columnspan=2,
+                           sticky=(constants.W), padx=5, pady=5)
 
-        username.grid(row=1, column=0)
-        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W))
+        # nämä parametrit voidaan poistaa tarvittaessa
+        username.grid(row=1, column=0, padx=5, pady=5)
+        username_entry.grid(row=1, column=1, sticky=(
+            constants.E, constants.W), padx=5, pady=5)
 
-        password.grid(row=2, column=0)
-        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W))
+        password.grid(row=2, column=0, padx=5, pady=5)
+        password_entry.grid(row=2, column=1, sticky=(
+            constants.E, constants.W), padx=5, pady=5)
 
         login_button.grid(row=3, column=0, columnspan=2,
-                          sticky=(constants.E, constants.W))
+                          sticky=(constants.E, constants.W), padx=5, pady=5)
 
         # Sarakkeet ottavat kaiken jäljelle jäävän tilan, kun ikkunan kokoa muutetaan
         # yhdessä elementtien sticky-parametrien kanssa
-        self._root.grid_columnconfigure(1, weight=1)
+        self._root.grid_columnconfigure(1, weight=1, minsize=300)
