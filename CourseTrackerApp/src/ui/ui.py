f3a84416008f99403ac51f4633371fc9256f56d5
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-03-26 18:13:54 +0100

    lisää tkinter harjoittelua

--- CourseTrackerApp/src/ui/ui.py
+++ CourseTrackerApp/src/ui/ui.py
@@ -1,4 +1,4 @@
-from tkinter import ttk
+from tkinter import ttk, constants
 
 # Harjoitellaan vielä UI:n rakentamista - tod.näk erotetaan kaikki näkymät omiksi luokikseen,
 # joita UI käyttää lopulta.
@@ -25,11 +25,18 @@ class UI:
         # Testataan kirjautumispainikkeen lisäämistä
         login_button = ttk.Button(master=self._root, text="Login")
 
-        heading_label.grid(row=0, column=0, columnspan=2)
+        # Harjoitellaan gridin luomista näkymään
+        heading_label.grid(row=0, column=0, columnspan=2, sticky=(constants.W))
 
         username.grid(row=1, column=0)
-        username_entry.grid(row=1, column=1)
+        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W))
 
         password.grid(row=2, column=0)
-        password_entry.grid(row=2, column=1)
-        login_button.grid(row=3, column=0, columnspan=2)
+        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W))
+
+        login_button.grid(row=3, column=0, columnspan=2,
+                          sticky=(constants.E, constants.W))
+
+        # Sarakkeet ottavat kaiken jäljelle jäävän tilan, kun ikkunan kokoa muutetaan
+        # yhdessä elementtien sticky-parametrien kanssa
+        self._root.grid_columnconfigure(1, weight=1)
