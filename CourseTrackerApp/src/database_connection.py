d1c560c971d25852aa811f59cd958eb75571a721
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-03-30 20:18:34 +0200

    alustetaan tietokannan luomista ja .env/python-dotenv:in lisäämistä

--- CourseTrackerApp/src/database_connection.py
+++ CourseTrackerApp/src/database_connection.py
@@ -3,8 +3,10 @@ import sqlite3
 
 dirname = os.path.dirname(__file__)
 
-connection = sqlite3.connect(os.path.join(
-    __dirname, "..", "data", "database.sqlite"))
+# nämä on vielä kova koodattu, muista vaihtaa!
+# connection = sqlite3.connect(os.path.join(
+#   __dirname, "..", "data", "database.sqlite"))
+connection = sqlite3.connect("practice.db")
 connection.row_factory = sqlite3.Row
 
 
