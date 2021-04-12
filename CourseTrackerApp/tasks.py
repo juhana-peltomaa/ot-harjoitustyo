6f0f986f2ce4c156cfbfad7b1b3a4305a0c1649e
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-04-12 16:16:36 +0200

    lisätään pytests tasks.py

--- CourseTrackerApp/tasks.py
+++ CourseTrackerApp/tasks.py
@@ -4,23 +4,34 @@ from invoke import task
 # Run the application with command "poetry run invoke start"
 @task
 def start(ctx):
-   ctx.run("python3 src/index.py")
+    ctx.run("python3 src/index.py")
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
 
 # Run coverage and generate html report with command "poetry run invoke coverage-report"
+
+
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest")
+
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html")
 
 # Run pylint checks with command "poetry run invoke lint"
+
+
 @task
 def lint(ctx):
     ctx.run("pylint src")
 
 # Run autopep8 formation with command "poetry run invoke format"
+
 
 @task
 def format(ctx):
