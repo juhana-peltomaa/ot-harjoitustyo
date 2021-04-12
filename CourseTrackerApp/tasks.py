b990358ffcb31725c9534ca20825129b2971c286
juhana-peltomaa <55188494+juhana-peltomaa@users.noreply.github.com>
2021-04-12 14:21:05 +0200

    Updated tasks.py lisäämällä test -komento

--- CourseTrackerApp/tasks.py
+++ CourseTrackerApp/tasks.py
@@ -5,6 +5,11 @@ from invoke import task
 @task
 def start(ctx):
    ctx.run("python3 src/index.py")
+   
+# Run the application tests
+@task
+def test(ctx):
+    ctx.run("pytest src")
 
 # Run coverage and generate html report with command "poetry run invoke coverage-report"
 @task
